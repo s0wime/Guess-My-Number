772483ef7ebb4b0180665802d3106d3e4833b687
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-09 23:24:11 +0200

    add event listener to guess button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
